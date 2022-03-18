894fec12c7fd3bb3bcc83ff4385d029435ea3db1
Longngok <bboylongngok193@gmail.com>
2022-03-18 14:24:49 +0700

    first commit

---
+++ Hw8 JS/index.js
@@ -0,0 +1,29 @@
+
+// // Bai 2
+// function min(a, b) {
+//     if (a < b) {
+//       return a;
+//     } else {
+//       return b;
+//     }
+//   }
+
+// // Bai 4
+// let username = prompt("Ban can dang nhap")
+// if(username == "Admin"){
+//     let password = prompt("ban can nhap mat khau")
+//     if(password == "mindX"){
+//         console.log("Chao Mung")
+//     }else
+//     if(password == "")
+//     {
+//         console.log("da huy")
+//     }else{
+//         console.log("Mat Khau sai")
+//     }
+// }else
+// if(username == ""){
+//     console.log("Cancelled")
+// }else{
+//     console.log("Toi khong biet ban")
+// }
