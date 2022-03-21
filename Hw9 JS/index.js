d56d86520ec57cf92ddb688878e966b229ce0602
Longngok <bboylongngok193@gmail.com>
2022-03-21 13:48:15 +0700

    first commit

---
+++ Hw9 JS/index.js
@@ -0,0 +1,35 @@
+// Hw1
+// let person = {
+//     name:       'Bob',
+//     occupation: 'web developer',
+//     hobbies:    'painting',
+//   };
+
+// c1 : console.log(person.name) 
+// c2 : console.log(person['name'])
+// c3 : console.log({name} = person)
+
+// Hw2
+// let obj = {
+//     b: 2,
+//     a: 1,
+//     c: 3,
+//   };
+// let objKey = Object.keys(obj);
+// let arr = [];
+// arr.push(objKey);
+// const upper = arr.map(element => {
+//   return element.toUpperCase();
+// });
+// console.log(upper)
+
+
+// Hw3
+// let obj = {
+//     foo: { a: "hello", b: "world" },
+//     bar: ["example", "mem", null, { xyz: 6 }, 88],
+//     qux: [4, 8, 12]
+//   };
+// c1 : obj.bar[3] = {xyz: 606}
+// c2 : obj.bar[3].xyz = 606;
+// console.log(obj)
