4504d0e30d276643c55aa651cb9ccd0b0dc3ecf6
Longngok <bboylongngok193@gmail.com>
2022-04-03 23:23:02 +0700

    first commit

---
+++ ProjectA/index.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.target', {
+    strings: [
+        "Try we're style and u can make your self good",
+        " And you can make it go true"
+    ],
+    typeSpeed: 100,
+    backSpeed: 30,
+    backDelay: 500,
+    startDelay: 1000,
+    loop : true,
+  });
