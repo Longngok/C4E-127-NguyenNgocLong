d4accc07fc257e7e7a26307cae8776ad59193844
Longngok <bboylongngok193@gmail.com>
2022-04-05 21:28:11 +0700

    first commit

--- ProjectA/index.js
+++ ProjectA/index.js
@@ -10,10 +10,3 @@ var typed = new Typed('.target', {
     loop : true,
   });
 
-const lowcapSw = {
-    name : "LOW-GAUGE KNIT SWEATER (NUDE IVORY)",
-    price : "$350.00",
-    info : "As part of our Spring-Summer 2022 collection, this drop features the low-gauge knit sweater in two colorways.<br>"
-};
-const lowcaps = document.getElementById("sweaterLow")
-lowcaps.innerHTML = lowcapSw.name;
